0ce21b7850f82b138e65bbb98042de4f1af934e8
algife <algife@gmail.com>
2023-11-23 21:16:57 +0100

    Preparing next config to accept images from external hosts

--- next.config.js
+++ next.config.js
@@ -3,6 +3,28 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "serpapi.com",
+        port: "",
+        pathname: "/searches/**",
+      },
+      {
+        protocol: "https",
+        hostname: "encrypted-tbn0.gstatic.com",
+        port: "",
+        pathname: "/images/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.imagin.studio",
+        port: "",
+        pathname: "/getImage/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
