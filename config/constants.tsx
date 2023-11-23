e03b4fcdeb933594396a139011f96664ee148d3b
algife <algife@gmail.com>
2023-11-23 19:28:13 +0100

    Adding constants that will be consumed by the search feature

--- config/constants.tsx
+++ config/constants.tsx
@@ -1,3 +1,7 @@
+import { CarSearchFilterPropsStrict } from "@/typings.d";
+
+export const ITEMS_PER_PAGE = 5;
+
 export const footerLinks = [
   {
     title: "About",
@@ -26,4 +30,58 @@ export const footerLinks = [
       { title: "Facebook", url: "/" },
     ],
   },
+];
+
+export const keysSearchFilters: (keyof CarSearchFilterPropsStrict)[] = [
+  // keys from
+  "model",
+  "manufacturer",
+  "year",
+  "fuel",
+  // "limit",
+  "page",
+];
+
+export const manufacturers = [
+  "Acura",
+  "Alfa Romeo",
+  "Aston Martin",
+  "Audi",
+  "Bentley",
+  "BMW",
+  "Buick",
+  "Cadillac",
+  "Chevrolet",
+  "Chrysler",
+  "Citroen",
+  "Dodge",
+  "Ferrari",
+  "Fiat",
+  "Ford",
+  "GMC",
+  "Honda",
+  "Hyundai",
+  "Infiniti",
+  "Jaguar",
+  "Jeep",
+  "Kia",
+  "Lamborghini",
+  "Land Rover",
+  "Lexus",
+  "Lincoln",
+  "Maserati",
+  "Mazda",
+  "McLaren",
+  "Mercedes-Benz",
+  "MINI",
+  "Mitsubishi",
+  "Nissan",
+  "Porsche",
+  "Ram",
+  "Rolls-Royce",
+  "Subaru",
+  "Tesla",
+  "Toyota",
+  "Volkswagen",
+  "Volvo",
 ];
